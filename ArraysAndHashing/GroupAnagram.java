0e1e96fea7a5b7b5b21c946f020c6acbb974f101
loc.trinh <loc.trinh@flownetworks.io>
2025-09-24 23:43:34 +0700

    String: GroupAnagram

--- ArraysAndHashing/GroupAnagram.java
+++ ArraysAndHashing/GroupAnagram.java
@@ -12,16 +12,33 @@ public class GroupAnagram {
   }
 
   public static List<List<String>> groupAnagrams(String[] strs) {
-    Map<String, ArrayList<String>> hashMap = new HashMap<>();
-    for (String str : strs) {
-      char[] chars = str.toCharArray();
-      Arrays.sort(chars);
-      if (hashMap.containsKey(String.valueOf(chars))) {
-        hashMap.get(String.valueOf(chars)).add(str);
-      } else {
-        hashMap.put(String.valueOf(chars), new ArrayList<>());
-        hashMap.get(String.valueOf(chars)).add(str);
+//    Map<String, ArrayList<String>> hashMap = new HashMap<>();
+//    for (String str : strs) {
+//      char[] chars = str.toCharArray();
+//      Arrays.sort(chars);
+//      if (hashMap.containsKey(String.valueOf(chars))) {
+//        hashMap.get(String.valueOf(chars)).add(str);
+//      } else {
+//        hashMap.put(String.valueOf(chars), new ArrayList<>());
+//        hashMap.get(String.valueOf(chars)).add(str);
+//      }
+//    }
+//    return new ArrayList<>(hashMap.values());
+
+    Map<String, List<String>> hashMap = new HashMap<>();
+
+    for (String s : strs) {
+      int[] strKey = new int[26];
+
+      for (char c : s.toCharArray()) {
+        strKey[c - 'a']++;
       }
+
+      String newKey = Arrays.toString(strKey);
+      if (!hashMap.containsKey(newKey)) {
+        hashMap.put(newKey, new ArrayList<>());
+      }
+      hashMap.get(newKey).add(s);
     }
     return new ArrayList<>(hashMap.values());
   }
