eb0a69750e8fe9652afc201763eafd3728eb9863
loc.trinh <loc.trinh@flownetworks.io>
2025-09-25 00:14:37 +0700

    Array: ProductExceptSelf

--- ArraysAndHashing/ProductExceptSelf.java
+++ ArraysAndHashing/ProductExceptSelf.java
@@ -1,5 +1,4 @@
 public class ProductExceptSelf {
-
   public int[] productExceptSelf(int[] nums) {
     int[] res = new int[nums.length];
     res[0] = 1;
@@ -7,11 +6,21 @@ public class ProductExceptSelf {
       res[i] = res[i - 1] * nums[i - 1];
     }
 
-    int subValue = 1;
-    for (int j = nums.length - 1; j >= 0; j--) {
-      res[j] *= subValue;
-      subValue *= nums[j];
+    int productNum = 1;
+    for (int i = nums.length - 1; i >= 0; i--) {
+      res[i] = res[i] * productNum;
+      productNum = nums[i] * productNum;
     }
     return res;
   }
 }
+
+/*
+input: nums = [1,2,3,4]
+output: res = [20,12,8,6]
+
+res: [1,1,2,6]
+i=2, productNum = 4 => res: [1,1,8,6]
+i=1, productNum = 12 => res: [1,12,8,6]
+i=0, productNum = 24 => res [24,12,8,6]
+ */
