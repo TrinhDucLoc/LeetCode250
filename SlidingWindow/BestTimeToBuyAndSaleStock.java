039f2682a02ff8f38c655e83ba589a0a6c722d3d
loc.trinh <loc.trinh@flownetworks.io>
2025-10-07 15:43:00 +0700

    Leetcode150

--- SlidingWindow/BestTimeToBuyAndSaleStock.java
+++ SlidingWindow/BestTimeToBuyAndSaleStock.java
@@ -1,13 +1,12 @@
 public class BestTimeToBuyAndSaleStock {
 
+  //prices = [7,1,5,3,6,4] -> 5
   public int maxProfit(int[] prices) {
-    int l = 0, r = 1;
-    int maxProfit = 0;
+    int l = 0, r = 1, maxProfit = 0;
 
     while (r < prices.length) {
       if (prices[l] < prices[r]) {
-        int profit = prices[r] - prices[l];
-        maxProfit = Math.max(profit, maxProfit);
+        maxProfit = Math.max(maxProfit, prices[r] - prices[l]);
       } else {
         l = r;
       }
