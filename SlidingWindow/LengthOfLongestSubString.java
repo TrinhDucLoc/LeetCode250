9213faa4e8fbac8d231034a5fd397f37c343ab78
loc.trinh <loc.trinh@flownetworks.io>
2025-10-07 15:52:16 +0700

    Leetcode150

--- SlidingWindow/LengthOfLongestSubString.java
+++ SlidingWindow/LengthOfLongestSubString.java
@@ -2,15 +2,22 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LengthOfLongestSubString {
-  public int lengthOfLongestSubstring(String s) {
+
+  public static void main(String[] args) {
+    System.out.println(lengthOfLongestSubstring("abcabcbb"));
+  }
+
+  // s = "abcabcbb" -> 3
+  public static int lengthOfLongestSubstring(String s) {
     Set<Character> hashSet = new HashSet<>();
     int l = 0, res = 0;
-    for (int i = 0; i < s.length(); i++) {
-      while (hashSet.contains(s.charAt(i))) {
+
+    for (int r = 0; r < s.length(); r++) {
+      while (hashSet.contains(s.charAt(r))) {
         hashSet.remove(s.charAt(l));
         l++;
       }
-      hashSet.add(s.charAt(i));
+      hashSet.add(s.charAt(r));
       res = Math.max(res, hashSet.size());
     }
     return res;
